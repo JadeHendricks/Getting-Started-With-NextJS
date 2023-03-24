4d1a63fde59835c15cbc3e7f3dcecbe771d72b0c
Jade <hendricksjade@gmail.com>
2023-03-24 15:26:26 +0200

    add(getting-started): Setting up the initial nextjs project and dependencies.

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Welcome</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, ipsam. Assumenda suscipit nemo debitis ut ducimus blanditiis nihil aliquid saepe.</p>
+        </>
+    )
+}
