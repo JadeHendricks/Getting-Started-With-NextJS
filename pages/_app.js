b190892ad0ba394cc7887a0c9d2de0ede877fcc0
Jade <hendricksjade@gmail.com>
2023-03-24 15:58:47 +0200

    add(navigation): Adding in a navigation and using the "useRouter" to determine what page is active.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import "../styles/global.css"
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
+
     return (
         <>
-            <p>Header</p>
-                <Component {...pageProps} />
+            <div>
+                <h1>OurSite</h1>
+                <nav className="header-nav">
+                    <ul>
+                        <li><Link className={router.pathname == "/" ? "active": ""} href="/">Home</Link></li>
+                        <li><Link className={router.pathname == "/about" ? "active": ""} href="/about">About</Link></li>
+                    </ul>
+                </nav>
+            </div>
+            <Component {...pageProps} />
             <p>Footer</p>
         </>
     )
