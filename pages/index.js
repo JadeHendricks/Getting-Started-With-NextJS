3b473f69ed7f619b6274eaf100f59ca975f440f8
Jade <hendricksjade@gmail.com>
2023-03-24 21:24:34 +0200

    add(get-server-side-props): Taking a look into what getServerSideProps does.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import Link from "next/link";
-
-export default function Home() {
+export default function Home(props) {
     return (
-        <>
-            <h1>Welcome</h1>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Ducimus, ipsam. Assumenda suscipit nemo debitis ut ducimus blanditiis nihil aliquid saepe.
-            </p>
-            <Link href="/about">Visit about page</Link>
-        </>
+      <div>
+        <h2>Welcome to our homepage.</h2>
+        <p>This is the best home page in the world. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aspernatur illum architecto cumque recusandae fuga sequi necessitatibus deleniti repellat harum nobis, dolor veniam vero deserunt. Voluptatibus, ducimus deserunt. Recusandae, dolore.</p>
+        <p>The weather: {props.forecast}</p>
+      </div>
     )
+  }
+  
+//if the data is super specific and not wanted to be cached then you can use this dynamic method
+//not the fastest approach, but this is what you do when you need something truly dynamic - this runs on every request
+export async function getServerSideProps() {
+    const response = await fetch("https://api.weather.gov/gridpoints/MFL/109,49/forecast")
+    const data = await response.json()
+
+    return {
+        props: {
+        forecast: data.properties.periods[0].detailedForecast
+        }
+    }
 }
